82c1f87df3b9c22464607cf198a6173cf1ffc861
Khurshida-Meem <meemrumi59@gmail.com>
2023-08-21 22:14:57 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import moment from 'moment';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const today = moment().format('YYYY-MM-DD');
+  const [fromDate, setFromDate] = useState('');
+  const [toDate, setToDate] = useState('');
+
+  const handleFromDateChange = (event) => {
+    setFromDate(event.target.value);
+  };
+
+  const handleToDateChange = (event) => {
+    setToDate(event.target.value);
+  };
+
+  const calcDateDiff = (fDate, tDate) => {
+    const difference  = moment(tDate).diff(moment(fDate), 'days');
+    return difference;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      console.log(calcDateDiff(fromDate ,today ))
+      console.log(fromDate, today)
+    }}>
+      <label htmlFor="fromDate">From Date:</label>
+      <input
+        type="date"
+        id="fromDate"
+        name="fromDate"
+        max={today}
+        value={fromDate}
+        onChange={handleFromDateChange}
+      />
+
+      <label htmlFor="toDate">To Date:</label>
+      <input
+        type="date"
+        id="toDate"
+        name="toDate"
+        min={fromDate}
+        max={calcDateDiff(fromDate ,today ) < 10 ? today : moment(fromDate).add(10, 'days').format('YYYY-MM-DD')}
+        value={toDate}
+        onChange={handleToDateChange}
+      />
+      <button type='submit'>submit</button>
+    </form>
   );
 }
 
